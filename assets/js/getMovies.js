20a5ce53c2b9319c4631c70ba0075c38793885ad
Vincentius Hadianta <hadiantavincent@gmail.com>
2023-02-08 00:42:48 +0700

    Input Poster

--- assets/js/getMovies.js
+++ assets/js/getMovies.js
@@ -1,4 +1,5 @@
 console.log("Local Storage, getMovies: ", localStorage.getItem("genreId"));
+
 let moviesList = document.getElementById(`movies`);
 let genreId = localStorage.getItem("genreId")
 
@@ -51,25 +52,121 @@ async function getMovieByGenre(genreId) {
 
     let detailMovie = mList[1][0]
 
-
-
-    mList.forEach(res => {
-        console.log(res);
+    mList.forEach(async res => {
+        // console.log("res", res);
         let detail = Object.entries(res)
-        console.log(detail);
-        detail.forEach(det => {
-            let key = det[0]
-            let value = det[1]
-            console.log(key, ": ", value)
 
-            moviesList.innerHTML +=
-            `<li>
-                <a href="movies.html" id="${detail[3]}" onclick="reply_click(this.id)">
-                    ${key}: ${value}
-                </a>
-            </li>`;
-            
-            // console.log("Movie: ", detail[det][1][0], detail[det][1][1]);
-        });
+        console.log("detail: ", detail);
+        // let movieId = detail[3]
+
+        // detail.forEach(det => {
+        // let key = det[0]
+        // let value = det[1]
+
+        let movieId = detail[3]
+        let title = detail[5]
+        let synopsis = detail[6]
+        // console.log(key, ": ", value)
+        // console.log("movieId: ", movieId[1])
+        let imageLink = await getImage(movieId[1])
+        console.log("imageLink: ",imageLink);
+        // console.log("getImage:: ",await getImage(movieId[1]))
+
+        moviesList.innerHTML +=
+            `<div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="${imageLink}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${synopsis}</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                    </div>
+                </div>
+            </div>`
+
+        //     `<div class="card" style="width: 18rem;">
+        //     <img src="..." class="card-img-top"alt="...">
+        //     <div class="card-body">
+        //       <h5 class="card-title">${title}</h5>
+        //       <p class="card-text container vertical-scrollable w-100">${synopsis}</p>
+        //       <a href="#" class="btn btn-primary">Go somewhere</a>
+        //     </div>
+        //   </div>`
+
+        // `<div>
+        // <a href="movies.html" id="${movieId}" onclick="reply_click(this.id)">
+        //     ${title}
+        // </a>
+        // ${synopsis}
+        // <div>`;
+
+        // console.log("Movie: ", detail[det][1][0], detail[det][1][1]);
+        // });
     });
 }
+
+async function getImage(movieId) {
+    let BASE_URL = `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=2687b1e38c449c31c16ae0f7f801fd75`
+    const IMAGE_URL = `https://image.tmdb.org/t/p/w185`
+
+    let promise = await fetch(BASE_URL)
+        .then(response => {
+            if (response.ok) {
+                return (response.json());
+            } else {
+                console.log(`getImage error: `, response.status)
+            }
+        })
+    console.log(`getImage promise: `, promise);
+    // sortAspect(promise)
+    console.log(`getImage entries: `, Object.entries(promise));
+
+    let imageDetail = Object.entries(promise)
+    console.log("imageDetail: ",imageDetail);
+    console.log("imageDetail[3]: ",imageDetail[3]);
+    console.log("imageDetail[3][1]: ",imageDetail[3][1]);
+    let posterList = imageDetail[3][1]
+
+    let link =[]
+    await posterList.forEach(e => {
+        console.log('e: ',e);
+        console.log('Object.entries(e): ',Object.entries(e));
+        console.log('Object.entries(e): ',Object.entries(e)[1]);
+        console.log('Object.entries(e): ',Object.entries(e)[1][1]);
+        console.log('Object.entries(e) 1250: ',Object.entries(e)[1][1].hasOwnProperty(1250)); //
+        console.log('e indexof: ',Object.entries(e).indexOf("height"));
+        if(Object.entries(e)[1][1]){ // == '1250'
+            console.log("OK: ",Object.entries(e)[3][1]);
+            link.push(Object.entries(e)[3][1])
+        }
+    });
+
+    console.log("link: ",link);
+    return IMAGE_URL+link[1]
+
+    // async function showImage(link){
+    //     let promise = await fetch(IMAGE_URL+link[1])
+    //         .then(e =>{
+    //             console.log("promise link img: ", e);
+    //         })
+    // }
+    // showImage(link)
+
+    // console.log(`getImage: `, await promise);
+    // return promise
+}
+
+// function sortAspect(imageObject) {
+//     let toSearch = 750
+//     for (var i = 0; i < imageObject.length; i++) {
+//         for (key in imageObject[i]) {
+//             if (imageObject[i][key].indexOf(toSearch) != -1) {
+//                 results.push(imageObject[i]);
+//             }
+//         }
+//     }
+// }
